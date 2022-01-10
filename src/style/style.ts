625c30045b290cb62396b89020d9ce4d0695e301
j8seangel <jseangel@gmail.com>
2022-01-10 23:15:02 +0100

    avoid crash when no light recalculate

--- src/style/style.ts
+++ src/style/style.ts
@@ -430,7 +430,9 @@ class Style extends Evented {
             }
         }
 
-        this.light.recalculate(parameters);
+        if (this.light) {
+            this.light.recalculate(parameters);
+        }
         this.z = parameters.zoom;
 
         if (changed) {
